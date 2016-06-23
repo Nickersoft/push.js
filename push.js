9d3b57594f55c0dd9bd57134c227bc43e9feed7c
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2016-06-23 20:38:59 -0300

    Fixed problem of undefined variable (#50)
    The isFunction method was using a variable that does not exist instead of using that is being passed as parameter.

--- push.js
+++ push.js
@@ -63,9 +63,7 @@
         self = this,
         isUndefined   = function (obj) { return obj === undefined; },
         isString   = function (obj) { return String(obj) === obj },
-        isFunction = function (obj) { return functionToCheck && 
-            {}.toString.call(functionToCheck) === '[object Function]'; 
-        },
+        isFunction = function (obj) { return obj && {}.toString.call(obj) === '[object Function]'; },
         
         /* Message to show if there is no suport to Push Notifications */
         incompatibilityErrorMessage = 'PushError: push.js is incompatible with browser.',
