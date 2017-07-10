dca18910bdd959d523e4ab07367aa1b07202d4a6
Tyler Nickerson <nickersoft@gmail.com>
2017-07-10 15:40:37 -0400

    Added link option

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ function demo() {
     Push.create('Hello world!', {
         body: 'How\'s it hangin\'?',
         icon: '/images/icon.png',
+        link: '/',
         timeout: 4000,
         onClick: function () {
             console.log("Fired!");
