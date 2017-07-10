16cb581e310e0847e85ec4057d8f2ceac6e58da5
Tyler Nickerson <nickersoft@gmail.com>
2017-07-10 13:24:45 -0400

    Fixed ServiceWorker issue on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 var button = document.getElementById("demo_button");
 
 function demo() {
-    Push.config({
-        serviceWorker: '/scripts/serviceWorker.js'
-    });
-
     Push.create('Hello world!', {
         body: 'How\'s it hangin\'?',
         icon: '/images/icon.png',
